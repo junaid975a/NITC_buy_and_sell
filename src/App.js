71855df0be9b60735d24ef81e4e81382486285c1
joel-ralte <joel.lr.ralte@gmail.com>
2023-10-06 18:57:38 +0530

    Integrated context with profile

--- src/App.js
+++ src/App.js
@@ -16,86 +16,84 @@ import Contact from "./pages/Contact";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AuthContext from "./context/auth/AuthContext";
+import AuthState from "./context/auth/AuthState";
 import ChatState from "./context/chat/ChatState";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const {isAuthenticated,setIsAuthenticated} = useContext(AuthContext)
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
   useEffect(() => {
     const localToken = localStorage.getItem("token");
     if (localToken) {
       setIsAuthenticated(true);
     }
-  }, [])
-  
-
+  }, []);
 
   return (
-    <div className="h-[100vh] w-[100vw]">
-      <ToastContainer />
-      <Navbar
-        isLoggedIn={isLoggedIn}
-        setIsLoggedIn={setIsLoggedIn}
-      />
+    <AuthState>
+      <div className="h-[100vh] w-[100vw]">
+        <ToastContainer />
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/login"
-          element={<Login setIsLoggedIn={setIsLoggedIn} />}
-        />
-        <Route
-          path="/signup"
-          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
-        />
-        {/* <Route path="mylist" element={<MyList/>}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/login"
+            element={<Login setIsLoggedIn={setIsLoggedIn} />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+          />
+          {/* <Route path="mylist" element={<MyList/>}/>
         <Route path="/bought" element={<Bought/>}/> */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute isLoggedIn={isAuthenticated}>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/mylist"
-          element={
-            <PrivateRoute isLoggedIn={isAuthenticated}>
-              <MyList />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/bought"
-          element={
-            <PrivateRoute isLoggedIn={isAuthenticated}>
-              <Bought />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute isLoggedIn={isAuthenticated}>
-              <Profile />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/chatpage"
-          element={
-            <PrivateRoute isLoggedIn={isAuthenticated}>
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute isLoggedIn={isAuthenticated}>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/mylist"
+            element={
+              <PrivateRoute isLoggedIn={isAuthenticated}>
+                <MyList />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/bought"
+            element={
+              <PrivateRoute isLoggedIn={isAuthenticated}>
+                <Bought />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute isLoggedIn={isAuthenticated}>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/chatpage"
+            element={
+              <PrivateRoute isLoggedIn={isAuthenticated}>
                 <ChatPage />
-            </PrivateRoute>
-          }
-        />
-        {/* <Route path="/profile" element={<Profile />} /> */}
-      </Routes>
-    </div>
+              </PrivateRoute>
+            }
+          />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+      </div>
+    </AuthState>
   );
 }
 
